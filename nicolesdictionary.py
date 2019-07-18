73a93b80bf8b4b3bb4382dd1d27f91cc2adc26a8
nicomcd <nicomcd0210@gmail.com>
2019-07-18 11:38:16 -0400

    adding all my work

---
+++ nicolesdictionary.py
@@ -0,0 +1,34 @@
+friends = {
+    "Me":17,
+    "Manuel":17,
+    "Lizbeth":16,
+    "Wendy":17,
+    "Kate":15,
+    "Leslie":16,
+    "JT":18,
+    "Brandan":16,
+    "Minseong":15,
+    "Ayden": 17,
+    "Rina":16,
+    "Linda":14,
+    "Erin":14,
+    "Hannah":15,
+    "Lucy unnie":18,
+    "Christie":14,
+    "Kamyla":14,
+    "Anais":15,
+    "Rohan":18,
+    "Jisu":18,
+    "Izear":16,
+    "Eva":17,
+    "Liana":17,
+    "Eva":14,
+}
+
+#What kind of values can dictionaries hold??????????
+
+print(friends)
+print("")
+print(friends["Manuel"])
+print("")
+print(friends["Me"])
